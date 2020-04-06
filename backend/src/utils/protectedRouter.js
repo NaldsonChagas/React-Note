5d03a9d41696a6911b0e20c52d79073375bebed6
Naldson Chagas <naldson.bc@gmail.com>
2020-04-06 12:50:01 -0300

    controle de acesso em rotas e instalação do eslint

---
+++ backend/src/utils/protectedRouter.js
@@ -0,0 +1,9 @@
+const { withJWTAuthMiddleware } = require('express-kun');
+
+const express = require('express');
+const router = express.Router();
+
+const protectedRouter = withJWTAuthMiddleware(router,
+  process.env.Router);
+
+module.exports = protectedRouter;
