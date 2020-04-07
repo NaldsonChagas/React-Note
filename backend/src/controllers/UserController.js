d55c48713ba808b1e5c9328e140a7ebea57f50a2
Naldson Chagas <naldson.bc@gmail.com>
2020-04-07 10:08:04 -0300

    delete user

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -19,5 +19,30 @@ module.exports = {
       res.status(500).json({ message: 'The user could not be saved' })
       throw new Error("Can't save user")
     }
+  },
+  async delete (req, res) {
+    const { userid } = req.headers
+    const { password } = req.body
+
+    const userFinded = await User
+      .findOne({ where: { id: userid } })
+
+    if (userFinded) {
+      if (passwordUtil
+        .comparePassword(password, userFinded.password)) {
+        try {
+          User.destroy({ where: { id: userid } })
+          res.json({ message: 'User successfully deleted' })
+        } catch (e) {
+          res.status(500)
+            .json({ message: 'The user could not be deleted' })
+          throw new Error('The user could not be deleted')
+        }
+      } else {
+        res.status(401).json({ message: 'Wrong password' })
+      }
+    } else {
+      res.status(400).json({ message: 'Nonexistent user' })
+    }
   }
 }
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -3,6 +3,9 @@ const router = express.Router()
 
 const userController = require('../controllers/UserController')
 
+const protectedRouter = require('../utils/protectedRouter')(router)
+
 router.post('/', userController.save)
+protectedRouter.delete('/', userController.delete)
 
 module.exports = router
