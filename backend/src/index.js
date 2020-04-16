6dcb97dfd5310e38904be9dca08d1bd979ebad71
Naldson Chagas <naldson.bc@gmail.com>
2020-04-15 22:53:26 -0300

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json())
 
 const connection = require('./database/config')
