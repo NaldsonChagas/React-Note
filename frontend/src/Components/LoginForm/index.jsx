696da24ab060784ed38dc9841122d4d65b8cb5ae
Naldson Chagas <naldson.bc@gmail.com>
2020-04-15 23:09:02 -0300

    Conectando com a api e fazendo post para login

--- frontend/src/Components/LoginForm/index.jsx
+++ frontend/src/Components/LoginForm/index.jsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
 export default function LoginForm() {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+
+  const history = useHistory();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    api.post('/', { user, password }).then(() => {
+      history.push('/notes');
+    }, (err) => {
+      console.log(err.response.data);
+    });
+  }
+
   return (
-    <form>
+    <form onSubmit={(e) => handleSubmit(e)}>
       <div className="form-group">
         <input
           className="form-control"
           name="user"
           id="user"
           placeholder="Nome de usuário ou email"
+          onChange={(e) => setUser(e.target.value)}
         />
       </div>
       <div className="form-group">
         <input
           className="form-control"
           name="password"
+          type="password"
           id="password"
           placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
         />
       </div>
       <button
