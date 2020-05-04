9a2fd4f26d399697774d70e89c299d1eb6a2287d
Naldson Chagas <naldson.bc@gmail.com>
2020-05-04 18:05:07 -0300

    verificando existência da senha no middleware

--- backend/src/midlewares/CheckUserPassword.js
+++ backend/src/midlewares/CheckUserPassword.js
@@ -9,10 +9,16 @@ module.exports = async function (req, res, next) {
   const userFinded = await User
     .findOne({ where: { id: userid } })
 
-  if (passwordUtil
-    .comparePassword(password, userFinded.password)) {
-    next()
+  if (password) {
+    if (passwordUtil
+      .comparePassword(password, userFinded.password)) {
+      next()
+    } else {
+      res.status(401)
+        .json({ message: 'Senha incorreta' })
+    }
   } else {
-    res.status(401).json({ message: 'Wrong password' })
+    res.status(400)
+      .json({ message: 'Você precisa enviar sua senha' })
   }
 }
