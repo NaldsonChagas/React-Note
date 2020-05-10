f2f2320565a13a1edc6833c4f18b5711a156a72e
Naldson Chagas <naldson.bc@gmail.com>
2020-05-10 18:53:08 -0300

    conexão com o banco na inicialização do servidor

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,11 +10,6 @@ app.use(express.json())
 
 const connection = require('./database/config')
 
-connection
-  .authenticate()
-  .then((result) => console.log('Connection with db ok'))
-  .catch((err) => console.log(err))
-
 require('./database/models/User')
 require('./database/models/Note')
 
@@ -50,5 +45,12 @@ process.on('exit', () => {
 
 const port = process.env.PORT
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
+  console.log(`Server is listening on port ${port}`);
+
+  (function connectWithDb () {
+    connection
+      .authenticate()
+      .then((result) => console.log('Connection with db ok'))
+      .catch((err) => console.log(err))
+  })()
 })
