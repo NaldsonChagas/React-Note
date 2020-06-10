9b1bd96a37f497e6bdf9d6a99e9d149f02b7c4c9
Naldson Chagas <naldson.bc@gmail.com>
2020-06-10 14:34:44 -0300

    rotas no index

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Register from './Pages/Register';
+import Notes from './Pages/Notes';
+import Profile from './Pages/Profile';
+import ProtectedRouter from './Utils/ProtectedRouter';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.css';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <Switch>
+      <Route component={Home} path="/" exact />
+      <Route component={Register} path="/register" />
+      <ProtectedRouter component={Notes} path="/notes" />
+      <ProtectedRouter component={Profile} path="/profile" />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
